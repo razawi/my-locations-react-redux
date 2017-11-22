3148b029e90c64f3df7e79b3464982104cb9bdd9
razawi <raz.kronenberg@gmail.com>
2017-11-22 10:47:12 +0200

    basic UI skelaton for Locations

--- my-locations/src/containers/Locations.js
+++ my-locations/src/containers/Locations.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { addCategory } from '../actions'
-import Reducers from '../reducers'
+import * as actions from '../actions'
+import './Locations.css'
 
+const CurrentLocation = ({}) => {
+  return(
+      <div className="currentLocation">
+        <p> Current Location </p>
+      </div>
+    )
+  }
 
-const ViewLocationsMenue = () => {
-  // debugger;
+const LocationsMenue = ({}) => {
+  return(
+    <div className="locationsMenue">
+      <p> Locations Menue </p>
+    </div>
+  )
+}
 
+class MapComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="mapComponent"> 
+        <p> Map </p>
+      </div>
+    )
+  }
+}
+
+class LocationsPanel extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="locationsPanel"> 
+      <LocationsMenue />
+      <div class="wrapper">
+        <MapComponent />
+        <CurrentLocation />
+      </div>
+      </div>
+    )
+  }
 }
 
 export default connect(
-  (state) => ({categories: state.categories, 
-               locations: state.locations,
-               currentCategory: state.currentCategory,
-               currentLocation: state.currentLocation
-              }), {}
-)(ViewLocationsMenue)
+    function (state){
+      return({
+        categories : state.categories.list,
+        currentCategory: state.categories.current,
+        actionState : state.uiActions.actionState
+      })
+    },
+    {
+      addCategory: actions.addLocation,
+      removeCategory: actions.removeLocation,
+      editCategory: actions.editLocation,
+      viewCategory: actions.viewLocation
+    }
+)(LocationsPanel)
