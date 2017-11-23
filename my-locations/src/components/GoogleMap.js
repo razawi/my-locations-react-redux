df8380446ab6b63a717578cb008cfe74265ff85a
razawi <raz.kronenberg@gmail.com>
2017-11-23 11:48:12 +0200

    more correct package.js. need to retrive data from hand click on the map

--- my-locations/src/components/GoogleMap.js
+++ my-locations/src/components/GoogleMap.js
@@ -3,9 +3,12 @@ import React from 'react'
 import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
+// generated for this app
+const API_KEY ='AIzaSyDQZnfW1bvZwOo0tlHiUDVLBczlsMkeQrU'
+
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=" + API_KEY,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `100%` }} />,
     mapElement: <div style={{ height: `100%` }} />,
@@ -21,45 +24,25 @@ const MyMapComponent = compose(
   </GoogleMap>
 )
 
-class MyFancyComponent extends React.PureComponent {
-  state = {
-    isMarkerShown: false,
-  }
-
-  componentDidMount() {
-    this.delayedShowMarker()
-  }
-
-  delayedShowMarker = () => {
-    setTimeout(() => {
-      this.setState({ isMarkerShown: true })
-    }, 3000)
-  }
-
-  handleMarkerClick = () => {
-    this.setState({ isMarkerShown: false })
-    this.delayedShowMarker()
-  }
-
-  render() {
-    return (
-      <MyMapComponent
-        isMarkerShown={this.state.isMarkerShown}
-        onMarkerClick={this.handleMarkerClick}
-      />
-    )
-  }
-}
-
 export class MapComponent extends React.Component {
     constructor(props) {
       super(props);
     }
+
+
+    handleMarkerClick = (e) => {
+        console.log('handle marker click')
+        debugger;
+    }
   
     render(){
+        debugger;
       return (
         <div className="mapComponent"> 
-            <MyMapComponent isMarkerShown={false} />
+            <MyMapComponent
+                isMarkerShown={true}
+                onMarkerClick={this.handleMarkerClick}
+            />
         </div>
       )
     }
